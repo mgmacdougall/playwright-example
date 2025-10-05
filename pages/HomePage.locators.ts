5687438d330343475188a13d8cea0d66b6d4f8b4
Mike MacDougall <mgmacdougall@outlook.com>
2025-10-05 07:02:45 -0400

    Update of home page locators

--- pages/HomePage.locators.ts
+++ pages/HomePage.locators.ts
@@ -1,3 +1,5 @@
 export const homePageLocators = {
   gettingStarted: { value: '.getStarted_Sjon' },
+  apiPageTitle: { value: 'h1' },
+  apiLnk: { value: '.theme-layout-navbar-left a:nth-child(4)' },
 };
