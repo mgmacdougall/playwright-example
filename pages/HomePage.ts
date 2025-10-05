c5ffac3bdf73b4ea069823d68ad44c0bf9c14bcd
Mike MacDougall <mgmacdougall@outlook.com>
2025-10-05 07:03:37 -0400

    Add of additional locators

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -5,10 +5,15 @@ import { homePageLocators } from './HomePage.locators';
 
 export class HomePage {
   readonly gettingStarted: Locator;
+  readonly apiPageTitle: Locator;
+  readonly apiLink: Locator;
+  readonly page: Page;
 
-  constructor(private page: Page) {
+  constructor(page: Page) {
     this.page = page;
     this.gettingStarted = page.locator(homePageLocators.gettingStarted.value);
+    this.apiPageTitle = page.locator(homePageLocators.apiPageTitle.value);
+    this.apiLink = page.locator(homePageLocators.apiLnk.value);
   }
 
   async navigate() {
@@ -21,5 +26,25 @@ export class HomePage {
 
   async clickGetStarted() {
     await this.gettingStarted.click();
+  }
+
+  async clickLink(linkText: string) {
+    await this.apiLink.click();
+  }
+
+  async waitForPageLoad() {
+    await this.page.waitForLoadState('load');
+  }
+
+  async validatePageTItle(expectedTitle: string) {
+    const actualTitle = await this.apiPageTitle.textContent();
+    console.log(`Actual Title: "${actualTitle}"`);
+    if (actualTitle?.trim() !== expectedTitle) {
+      throw new Error(
+        `Page title does not match. Expected: "${expectedTitle}", Actual: "${actualTitle?.trim()}"`
+      );
+    } else {
+      console.log(`Page title matches: "${expectedTitle}"`);
+    }
   }
 }
